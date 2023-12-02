2cab06b6b97bbe3f516013265f66d784c8292ca1
Thanh Nguyen <thanhnguyen_2025@depauw.edu>
2023-12-02 14:42:33 -0500

    First commit

---
+++ src/firebase/init.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCWi8q2qxI8e3Ss1VWMQrCA-4fqm7mfdX8",
+  authDomain: "obsidian-web-viewer.firebaseapp.com",
+  projectId: "obsidian-web-viewer",
+  storageBucket: "obsidian-web-viewer.appspot.com",
+  messagingSenderId: "58884208947",
+  appId: "1:58884208947:web:a858f1578d0b2cdb222646",
+  measurementId: "G-84FJSNL2ME"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore()
+export default db
